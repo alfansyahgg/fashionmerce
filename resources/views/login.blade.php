a259a1909aad6ff0a155698d85965dc67830f21e
alfansyahgg <alfansyahpratama52@gmail.com>
2022-02-05 09:24:22 +0700

    Update SEO

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@ PT BERKAH CIPTA KARYA BERSAMA adalah perusahaan yang bergerak di bidang penyedia
 @endsection
 
 @section('keywords')
-seragam tni bandung, seragam dinas polri bandung, seragam tentara bandung, seragam pns bandung, jaket training bandung, konveksi seragam bandung, jasa pakaian bandung
+seragam tni bandung,seragam dinas polri bandung,seragam tentara bandung,seragam militer bandung,seragam pns bandung,jaket training bandung,konveksi seragam bandung,jasa pakaian bandung,pabrik seragam bandung,
 @endsection
 
 @section('title')
